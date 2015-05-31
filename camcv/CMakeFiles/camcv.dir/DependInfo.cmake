607770cb42b5c21e1a251e72d096c81668f37e5b
Arko <dotuplink@gmail.com>
2015-05-31 10:50:45 -0700

    ECE542_OPTIMIZED
    ECE542_OPTIMIZED

---
+++ camcv/CMakeFiles/camcv.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/RaspiCLI.c" "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/CMakeFiles/camcv.dir/RaspiCLI.c.o"
+  "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/RaspiCamControl.c" "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/CMakeFiles/camcv.dir/RaspiCamControl.c.o"
+  "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/RaspiPreview.c" "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/CMakeFiles/camcv.dir/RaspiPreview.c.o"
+  "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/RaspiTex.c" "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/CMakeFiles/camcv.dir/RaspiTex.c.o"
+  "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/RaspiTexUtil.c" "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/CMakeFiles/camcv.dir/RaspiTexUtil.c.o"
+  "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/gl_scenes/mirror.c" "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/CMakeFiles/camcv.dir/gl_scenes/mirror.c.o"
+  "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/gl_scenes/models.c" "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/CMakeFiles/camcv.dir/gl_scenes/models.c.o"
+  "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/gl_scenes/sobel.c" "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/CMakeFiles/camcv.dir/gl_scenes/sobel.c.o"
+  "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/gl_scenes/square.c" "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/CMakeFiles/camcv.dir/gl_scenes/square.c.o"
+  "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/gl_scenes/teapot.c" "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/CMakeFiles/camcv.dir/gl_scenes/teapot.c.o"
+  "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/gl_scenes/yuv.c" "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/CMakeFiles/camcv.dir/gl_scenes/yuv.c.o"
+  "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/tga.c" "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/CMakeFiles/camcv.dir/tga.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/camcv.cpp" "/home/pi/Code/Bullseye/RoboSub_Vision/camcv/CMakeFiles/camcv.dir/camcv.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "/opt/vc/userland-master/host_applications/linux/libs/bcm_host/include"
+  "/opt/vc/userland-master/host_applications/linux/apps/raspicam/gl_scenes"
+  "/opt/vc/userland-master/interface/vcos"
+  "/opt/vc/userland-master"
+  "/opt/vc/userland-master/interface/vcos/pthreads"
+  "/opt/vc/userland-master/interface/vmcs_host/linux"
+  "/opt/vc/userland-master/interface/khronos/include"
+  "/opt/vc/userland-master/interface/khronos/common"
+  "./gl_scenes"
+  "."
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
